cf4c012374dbab1f5dc0d541d20123108da26729
mihaylinsergey <semi999@mail.ru>
2023-07-18 17:08:48 +0300

    3.1. @RequestParam. Форма редактирования [#308708]

--- src/main/java/ru/job4j/accidents/controller/AccidentController.java
+++ src/main/java/ru/job4j/accidents/controller/AccidentController.java
@@ -3,10 +3,7 @@ package ru.job4j.accidents.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.service.AccidentService;
 
@@ -30,8 +27,8 @@ public class AccidentController {
         return "redirect:/index";
     }
 
-    @GetMapping("/{accidentId}")
-    public String getById(Model model, @PathVariable ("accidentId") int id) {
+    @GetMapping("/updateAccident")
+    public String getById(@RequestParam("id") int id, Model model) {
         var accidentOptional = accidentService.findById(id);
         model.addAttribute("accident", accidentOptional.get());
         return "updateAccident";
