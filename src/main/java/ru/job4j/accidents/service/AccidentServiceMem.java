e9291bc3aa8e1a06f3ff6616ed482bfba5f4535d
mihaylinsergey <semi999@mail.ru>
2023-07-24 10:37:01 +0300

    4. Form с композиционным объектом [#305522 #430837]

--- src/main/java/ru/job4j/accidents/service/AccidentServiceMem.java
+++ src/main/java/ru/job4j/accidents/service/AccidentServiceMem.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.model.Rule;
 import ru.job4j.accidents.repository.AccidentRepository;
+import ru.job4j.accidents.repository.AccidentTypeRepository;
+import ru.job4j.accidents.repository.RuleRepository;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Collection;
 import java.util.Optional;
@@ -17,8 +19,8 @@ import java.util.Set;
 public class AccidentServiceMem implements AccidentService {
 
     private final AccidentRepository accidentRepository;
-    private final AccidentTypeService accidentTypeService;
-    private final RuleService ruleService;
+    private final AccidentTypeRepository accidentTypeRepository;
+    private final RuleRepository ruleRepository;
 
     @Override
     public Collection<Accident> findAll() {
@@ -51,14 +53,14 @@ public class AccidentServiceMem implements AccidentService {
 
     private Accident getAccidentWithType(Accident accident) {
         int typeId = accident.getType().getId();
-        var type = accidentTypeService.findById(typeId).get();
+        var type = accidentTypeRepository.findById(typeId).get();
         accident.setType(type);
         return accident;
     }
 
     private Accident getAccidentWithRule(Accident accident, HttpServletRequest req) {
         String[] ids = req.getParameterValues("rIds");
-        Set<Rule> rule = ruleService.findById(ids);
+        Set<Rule> rule = ruleRepository.findById(ids);
         accident.setRules(rule);
         return accident;
     }
