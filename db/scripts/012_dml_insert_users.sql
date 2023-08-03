ba712e5637b62b5dced1420a4b70dd1c7ebbe687
mihaylinsergey <semi999@mail.ru>
2023-08-03 18:09:14 +0300

    0. Spring test [#6881 #434393]

---
+++ db/scripts/012_dml_insert_users.sql
@@ -0,0 +1,3 @@
+insert into users (username, enabled, password, authority_id)
+values ('root', true, '$2a$10$wY1twJhMQjGVxv4y5dBC5ucCBlzkzT4FIGa4FNB/pS9GaXC2wm9/W',
+(select id from authorities where authority = 'ROLE_ADMIN'));
